83841919ff49eea3cacc43b077be2dbc88a0abfe
Daniel Frick <daniellfrick@gmail.com>
2017-04-30 16:25:56 -0500

    rename mod

--- src/main/java/com/cowwithhead/elventrickery/ExampleMod.java
+++ src/main/java/com/cowwithhead/elventrickery/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.cowwithhead.elventrickery;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -8,7 +8,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "Elven Trickery";
     public static final String VERSION = "1.0";
     
     @EventHandler
